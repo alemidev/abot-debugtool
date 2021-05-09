3c496320d86b0d7b60e9e9ecf184f2b3c59708eb
alemi <ale.migliardi@gmail.com>
2021-05-09 22:26:09 +0200

    converted to new help system

--- debugtool.py
+++ debugtool.py
@@ -40,51 +40,56 @@ class stdoutWrapper():
 
 HELP = HelpCategory("DEBUGTOOL")
 
-HELP.add_help("put", "save file to server",
-				"reply to a media message or attach a media to this command to " +
-				"store a file on the bot root folder.")
+@HELP.add()
 @alemiBot.on_message(is_superuser & filterCommand("put", list(alemiBot.prefixes)))
 @report_error(logger)
 @set_offline
 async def put_cmd(client, message):
+	"""save file to server
+
+	Reply to a media message or attach media to store file on server
+	"""
 	msg = message
 	if message.reply_to_message is not None:
 		msg = message.reply_to_message
 	if msg.media:
-		logger.info("Downloading media")
 		fpath = await client.download_media(msg)
 		await edit_or_reply(message, '` → ` saved file as {}'.format(fpath))
 	else:
 		await edit_or_reply(message, "`[!] → ` No file")
 
-HELP.add_help("get", "request a file from server",
-				"will upload a file from server to this chat. The path can be " +
-				"global. Use flag `-log` to automatically include `/data/scraped_media`.",
-				args="[-log] <path>")
+@HELP.add(cmd="<path>")
 @alemiBot.on_message(is_superuser & filterCommand("get", list(alemiBot.prefixes), flags=["-log"]))
 @report_error(logger)
 @set_offline
 async def get_cmd(client, message):
+	"""request a file from server
+
+	Will upload a file from server to this chat.
+	The path can be absolute or relative (starting from alemiBot workdir).
+	Use flag `-log` to automatically upload `data/debug.log`.
+	"""
 	if "cmd" not in message.command:
 		return await edit_or_reply(message, "`[!] → ` No input")
-	logger.info("Uploading media")
 	await client.send_chat_action(message.chat.id, "upload_document")
 	name = message.command["cmd"][0]
 	if "-log" in message.command["flags"]: # this is handy for logger module!
-		name = "data/scraped_media/" + name
+		name = "data/debug.log" + name
 	await client.send_document(message.chat.id, name, reply_to_message_id=message.message_id, caption=f'` → {name}`')
 	await client.send_chat_action(message.chat.id, "cancel")
 
-HELP.add_help(["run", "r"], "run command on server",
-				"runs a command on server. Shell will be from user running bot. " +
-				"Every command starts in bot root folder. There is a timeout of 60 seconds " +
-				"to any command issued, this can be changed with the `-t` option. You should " +
-				"properly wrap your arguments with `\"`, they will be ignored by cmd parser.", args="[-t <n>] <cmd>")
+@HELP.add(cmd="<cmd>")
 @alemiBot.on_message(is_superuser & filterCommand(["run", "r"], list(alemiBot.prefixes), options={
 	"timeout" : ["-t", "-time"]
 }))
 @set_offline
 async def run_cmd(client, message):
+	"""run a command on server
+
+	Shell will be from user running bot: every command starts in bot root folder.
+	There is a timeout of 60 seconds to any command issued, this can be changed with the `-t` option.
+	You should properly wrap your arguments with `\"`, they will be ignored by cmd parser.
+	"""
 	args = re.sub(r"-delme(?: |)(?:[0-9]+|)", "", message.command["raw"])
 	msg = await edit_or_reply(message, "` → ` Running")
 	timeout = float(message.command["timeout"]) if "timeout" in message.command else 60.0
@@ -114,16 +119,16 @@ async def run_cmd(client, message):
 		logger.exception("Error in .run command")
 		await msg.edit(f"`$ {args}`\n`[!] → ` " + str(e))
 
-HELP.add_help(["eval", "e"], "eval a python expression",
-				"eval a python expression. No imports can be made nor variables can be " +
-				"assigned. Some common libs are already imported. `eval` cannot have side effects. " +
-				"Returned value will be printed upon successful evaluation. `stdout` won't be captured (use `.ex`). If " +
-				"a coroutine is returned, it will be awaited. No assignation can " +
-				"be done in `eval`, but getting fields is possible. This won't tokenize large outputs per-line, " +
-				"use .ex if you need that.", args="<expr>")
+@HELP.add(cmd="<expr>")
 @alemiBot.on_message(is_superuser & filterCommand(["eval", "e"], list(alemiBot.prefixes)))
 @set_offline
 async def eval_cmd(client, message):
+	"""eval a python expression
+
+	No imports can be made nor variables can be assigned :`eval` cannot have side effects.
+	Returned value will be printed upon successful evaluation. `stdout` won't be captured (use `.ex`).
+	If a coroutine is returned, it will be awaited. This won't tokenize large outputs per-line,	use .ex if you need that.
+	"""
 	args = re.sub(r"-delme(?: |)(?:[0-9]+|)", "", message.command["raw"])
 	msg = await edit_or_reply(message, "` → ` Evaluating")
 	try:
@@ -157,15 +162,16 @@ async def aexec(code, client, message): # client and message are passed so they 
 	)
 	return await locals()['__aex']()
 
-HELP.add_help(["exec", "ex"], "execute python code",
-				"execute python code. This, unlike `eval`, has no bounds and " +
-				"**can have side effects**. Use with more caution than `eval`. " +
-				"`exec` always returns `None`, but anything printed to `stdout` " +
-				"will be shown. The `exec` call is wrapped to make it work with async " +
-				"code.", args="<code>")
+@HELP.add(cmd="<code>")
 @alemiBot.on_message(is_superuser & filterCommand(["exec", "ex"], list(alemiBot.prefixes)))
 @set_offline
 async def exec_cmd(client, message):
+	"""execute python code
+
+	Will capture and print stdout.
+	This, unlike `eval`, has no bounds and **can have side effects**. Use with more caution than `eval`!
+	The `exec` call is wrapped to make it work with async code.
+	"""
 	args = re.sub(r"-delme(?: |)(?:[0-9]+|)", "", message.command["raw"])
 	fancy_args = args.replace("\n", "\n... ")
 	msg = message if is_me(message) else await message.reply("`[PLACEHOLDER]`")
@@ -191,14 +197,17 @@ async def exec_cmd(client, message):
 		logger.exception("Error in .exec command")
 		await msg.edit(f"`>>> {args}`\n`[!] → ` " + str(e), parse_mode='markdown')
 
-HELP.add_help("where", "get info about chat",
-				"Get complete information about a chat and send it as json. If no chat name " +
-				"or id is specified, current chat will be used. Add `-no` at the end if you just want the " +
-				"id : no file will be attached.", args="[<target>] [-no]", public=True)
+@HELP.add(cmd="[<target>]", sudo=False)
 @alemiBot.on_message(is_allowed & filterCommand("where", list(alemiBot.prefixes), flags=["-no"]))
 @report_error(logger)
 @set_offline
 async def where_cmd(client, message):
+	"""get info about a chat
+
+	Get complete information about a chat and send it as json.
+	If no chat name	or id is specified, current chat will be used.
+	Add `-no` at the end if you just want the id : no file will be attached.
+	"""
 	tgt = message.chat
 	if "cmd" in message.command:
 		arg = message.command["cmd"][0]
@@ -206,45 +215,50 @@ async def where_cmd(client, message):
 			tgt = await client.get_chat(int(arg))
 		else:
 			tgt = await client.get_chat(arg)
-	logger.info(f"Getting info of chat")
 	await edit_or_reply(message, f"` → ` Getting data of chat `{tgt.id}`")
 	if not "-no" in message.command["flags"]:
 		out = io.BytesIO((str(tgt)).encode('utf-8'))
 		out.name = f"chat-{tgt.id}.json"
 		await client.send_document(message.chat.id, out)
 
-HELP.add_help("who", "get info about user",
-				"Get complete information about user and attach as json. If replying to a message, author will be used. " +
-				"An id or @ can be specified. If neither is applicable, self will be used. Append `-no` if you just want the id.",
-				public=True, args="[<target>] [-no]")
+@HELP.add(cmd="[<target>]", sudo=False)
 @alemiBot.on_message(is_allowed & filterCommand("who", list(alemiBot.prefixes), flags=["-no"]))
 @report_error(logger)
 @set_offline
 async def who_cmd(client, message):
+	"""get info about a user
+
+	Get complete information about user and attach as json.
+	If replying to a message, author will be used.
+	An id or @ can be specified. If neither is applicable, self will be used.
+	Use `-no` flag if you just want the id.
+	"""
 	peer = get_user(message)
 	if "cmd" in message.command:
 		arg = message.command["cmd"][0]
 		peer = await client.get_users(int(arg) if arg.isnumeric() else arg)
 	elif message.reply_to_message is not None:
 		peer = get_user(message.reply_to_message)
-	logger.info("Getting info of user")
 	await edit_or_reply(message, f"` → ` Getting data of user `{peer.id}`")
 	if not "-no" in message.command["flags"]:
 		out = io.BytesIO((str(peer)).encode('utf-8'))
 		out.name = f"user-{peer.id}.json"
 		await client.send_document(message.chat.id, out)
 
-HELP.add_help("what", "get info about message",
-				"Get complete information about a message and attach as json. If replying, replied message will be used. "+
-				"id and chat can be passed as arguments. If no chat is specified with `-g`, " +
-				"message will be searched in current chat. Append `-no` if you just want the id.",
-				args="[<target>] [-g <chatId>] [-no]", public=True)
+@HELP.add(cmd="[<target>]", sudo=False)
 @alemiBot.on_message(is_allowed & filterCommand("what", list(alemiBot.prefixes), options={
 	"group" : ["-g", "-group"]
 }, flags=["-no"]))
 @report_error(logger)
 @set_offline
 async def what_cmd(client, message):
+	"""get info about a message
+
+	Get complete information about a message and attach as json.
+	If replying, replied message will be used.
+	Id and chat can be passed as arguments. If no chat is specified with `-g`, message will be searched in current chat.
+	Append `-no` if you just want the id.
+	"""
 	msg = message
 	if message.reply_to_message is not None:
 		msg = await client.get_messages(message.chat.id, message.reply_to_message.message_id)
@@ -256,36 +270,40 @@ async def what_cmd(client, message):
 			else:
 				chat_id = (await client.get_chat(message.command["group"])).id
 		msg = await client.get_messages(chat_id, int(message.command["cmd"][0]))
-	logger.info("Getting info of msg")
 	await edit_or_reply(message, f"` → ` Getting data of msg `{msg.message_id}`")
 	if not "-no" in message.command["flags"]:
 		out = io.BytesIO((str(msg)).encode('utf-8'))
 		out.name = f"msg-{msg.message_id}.json"
 		await client.send_document(message.chat.id, out)
 
-HELP.add_help(["joined", "jd"], "count active chats",
-				"get number of all dialogs : groups, supergroups, channels, dms, bots")
+@HELP.add()
 @alemiBot.on_message(is_superuser & filterCommand(["joined", "jd"], list(alemiBot.prefixes)))
 @report_error(logger)
 @set_offline
 async def joined_cmd(client, message):
-	logger.info("Listing active dialogs")
+	"""count active chats
+
+	Get number of dialogs: groups, supergroups, channels, dms, bots.
+	Will show them divided by category and a total.
+	"""
 	msg = await edit_or_reply(message, "` → ` Counting...")
 	res = {}
+	total = 0
 	async for dialog in client.iter_dialogs():
-		if dialog.chat.type in res:
-			res[dialog.chat.type] += 1
-		else:
-			res[dialog.chat.type] = 1
-	out = "`→ ` --Active chats-- \n"
+		if dialog.chat.type not in res:
+			res[dialog.chat.type] = 0
+		res[dialog.chat.type] += 1
+		total += 1
+	out = "`→ ` **{total}** --Active chats-- \n"
 	for k in res:
-		out += f"` → {k} ` {res[k]}\n"
+		out += f"` → ` **{k}** {res[k]}\n"
 	await msg.edit(out)
 
 @alemiBot.on_message(is_superuser & filterCommand(["make_botfather_list"], list(alemiBot.prefixes)))
 @report_error(logger)
 @set_offline
 async def botfather_list_command(client, message):
+	"""make botfather-compatible command list"""
 	out = ""
 	for k in CATEGORIES:
 		for kk in CATEGORIES[k].HELP_ENTRIES:
